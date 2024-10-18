d75fd22bd3d2c4c60189ea320e415e79fad870f9
saisito <said971219@gmail.com>
2024-10-18 12:48:49 -0300

    changing README.md

--- download_files.py
+++ download_files.py
@@ -2,15 +2,15 @@ import gdown
 import os
 
 files = [
-    {"id": "1cWmr7C3Ox0cK6wL5tr3PiDqEjd7X0iap", "name": "AmBe/AmBe_cdparafb.dat"},
-    {"id": "1zP-XywxZPinYCqDIOiGDJVFFVFC5driP", "name": "AmBe/AmBe_desnuda.dat"},
-    {"id": "1e8snPLFctL00qOLpH4u19wonhSISV7OF", "name": "AmBe/AmBe_plomo(10cm).dat"},
-    {"id": "16dxS3s9cxaUUxMKb38fe2ldsqkHUvgdy", "name": "AmBe/fondo_AmBe_cdparafb.dat"},
-    {"id": "1hKX2MDz0HU5yrvSax_uAY3TGOOjLKBMZ", "name": "AmBe/fondo_AmBe_desnuda.dat"},
-    {"id": "1wUPnZEpWPSPATU8YDUdXvozL0u-HFBA5", "name": "AmBe/fondo_AmBe_plomo(10cm).dat"},
+    {"id": "1cWmr7C3Ox0cK6wL5tr3PiDqEjd7X0iap", "name": "AmBe_cdparafb.dat"},
+    {"id": "1zP-XywxZPinYCqDIOiGDJVFFVFC5driP", "name": "AmBe_desnuda.dat"},
+    {"id": "1e8snPLFctL00qOLpH4u19wonhSISV7OF", "name": "AmBe_plomo(10cm).dat"},
+    {"id": "16dxS3s9cxaUUxMKb38fe2ldsqkHUvgdy", "name": "fondo_AmBe_cdparafb.dat"},
+    {"id": "1hKX2MDz0HU5yrvSax_uAY3TGOOjLKBMZ", "name": "fondo_AmBe_desnuda.dat"},
+    {"id": "1wUPnZEpWPSPATU8YDUdXvozL0u-HFBA5", "name": "fondo_AmBe_plomo(10cm).dat"},
 ]
 
-# Crear la carpeta AmBe si no existe
+# Crear la carpeta 'AmBe' si no existe
 if not os.path.exists('AmBe'):
     os.makedirs('AmBe')
 
@@ -19,8 +19,11 @@ for file in files:
     url = f"https://drive.google.com/uc?id={file['id']}"
     output = file["name"]
     
+    # Crear la ruta completa para guardar el archivo
     output_path = os.path.join('AmBe', output)
     
+    # Descargar el archivo
     gdown.download(url, output_path, quiet=False)
 
 print("All files have been downloaded")
+
